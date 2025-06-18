29593b6a00fc2fc46c44a66e3dcd02d6b5f7110e
YazmyratKakageldiyev <YazmyratKakageldiyev>
2025-06-18 09:06:07 +0500

    Add new finctions ContainsInt

--- utils.go
+++ utils.go
@@ -4,6 +4,15 @@ package utils
 
 func Contains(a []string, x string) bool {
 	for _, n := range a {
+		if n == x {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsInt(a []int, x int) bool {
+	for _, n := range a {
 		if x == n {
 			return true
 		}
